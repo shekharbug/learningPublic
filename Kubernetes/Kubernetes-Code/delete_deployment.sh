b2f18757ec445dac192f76331498e8b6ab47b7ec
shekhar <shekhar.bug@gmail.com>
2025-05-10 13:30:20 +0000

    deployment guide with loadbalancer

---
+++ Kubernetes/Kubernetes-Code/delete_deployment.sh
@@ -0,0 +1,9 @@
+kubectl get deployments flask-app-deployment
+kubectl delete deployments flask-app-deployment
+kubectl get pods
+kubectl get service
+kubectl delete service flask-app-service
+kubectl get deployments
+kubectl get pods
+kubectl get service
+
